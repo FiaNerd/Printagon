ba6d232fc516b99f3247922fb07c70c718b0d68a
fianerd <mattiasson.sofia@gmail.com>
2026-02-10 20:30:52 +0100

    feat(model): add Roll model

---
+++ Printagon.Api/Models/Roll.cs
@@ -0,0 +1,15 @@
+﻿namespace Printagon.Api.Models
+{
+    public class Roll
+    {
+        public  Guid Id { get; set; }
+        public string? PaperType { get; set; }
+        public int RollNumber { get; set; }
+        public int RollWeight { get; set; }
+        public string? Comment { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
