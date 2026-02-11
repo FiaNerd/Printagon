7893860cdf65a1b8d33c6cc0556eae39e4e3e89d
fianerd <mattiasson.sofia@gmail.com>
2026-02-11 19:25:46 +0100

    feat(context): add DbSet to context file

--- Printagon.Api/Data/ApplicationDbContext.cs
+++ Printagon.Api/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Printagon.Api.Models;
 
 namespace Printagon.Api.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Roll> Rolls { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
