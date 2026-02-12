4256fe9d19310273b5e3693d9aca38c018807c16
fianerd <mattiasson.sofia@gmail.com>
2026-02-12 16:22:37 +0100

    refactor(models): update Roll model with optional fields and metadata

--- Printagon.Api/Models/Roll.cs
+++ Printagon.Api/Models/Roll.cs
@@ -4,8 +4,12 @@
     {
         public  Guid Id { get; set; }
         public string? PaperType { get; set; }
+        public int? GramWeight { get; set; }
+        public int? RollWidth { get; set; }
+
         public int RollNumber { get; set; }
         public int RollWeight { get; set; }
+
         public string? Comment { get; set; }
         public Guid CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
