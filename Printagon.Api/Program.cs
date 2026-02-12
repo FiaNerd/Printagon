0a2ae2e5df8104354fb4745c0508dc6ccef2e5f6
fianerd <mattiasson.sofia@gmail.com>
2026-02-12 14:20:38 +0100

    feat(config): add InMeory database to pipline

--- Printagon.Api/Program.cs
+++ Printagon.Api/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Printagon.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("PrintagonDb"));
+
 
 var app = builder.Build();
 
@@ -12,7 +21,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapControllers();
+
 app.UseHttpsRedirection();
+
 
 
 app.Run();
