8b2c38dc0348ea928c1224462b0c672ffe1f435a
fianerd <mattiasson.sofia@gmail.com>
2026-02-27 05:12:01 +0100

    feat(seed-data): add dummy data to the project

---
+++ Printagon.Api/Data/Seed/DbSeeder.cs
@@ -0,0 +1,71 @@
+﻿using Printagon.Api.Models;
+
+namespace Printagon.Api.Data.Seed
+{
+    public class DbSeeder
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Orders.Any())
+            {
+                return;
+            }
+
+            /* -----------
+                ORDERS
+               -----------
+            */
+
+            var order1 = new Order
+            {
+                OrderNumber = 123,
+                JobName = "Ica",
+                YearlyNumber = 2,
+                PaperType = "Holmen View HS",
+                GramWeight = 53,
+                RollWidth = 1144,
+                OrderStatus = 0,
+                CreatedAt = DateTime.Now,
+                Rolls = new List<Roll> { }
+            };
+
+            /* -----------
+               ROLLS
+              -----------
+           */
+
+            var roll1 = new Roll
+            {
+                PaperType = "Holmen View HS",
+                GramWeight = 53,
+                RollWidth = 1144,
+                RollNumber = 1,
+                RollWeight = 1000,
+                Comment = "First roll",
+                CreatedBy = Guid.NewGuid(),
+                CreatedAt = DateTime.Now,
+                Orders = new List<Order> { order1 }
+
+            };
+
+            var roll2 = new Roll
+            {
+                PaperType = "Holmen View HS",
+                GramWeight = 53,
+                RollWidth = 1144,
+                RollNumber = 2,
+                RollWeight = 1000,
+                Comment = "Second roll",
+                CreatedBy = Guid.NewGuid(),
+                CreatedAt = DateTime.Now,
+                Orders = new List<Order> { order1 }
+            };
+            context.Orders.Add(order1);
+            context.Rolls.AddRange(roll1, roll2);
+            context.SaveChanges();
+
+        }
+    }
+}
