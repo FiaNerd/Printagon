de98d39cacc84829ea308036b87e3db846ae96df
fianerd <mattiasson.sofia@gmail.com>
2026-03-10 11:47:14 +0100

    feat(order-service): Add order serice file connect it to the order service interface

---
+++ Printagon.Api/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using Printagon.Api.DTOs.Order;
+
+namespace Printagon.Api.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<OrderResponseDto?> GetOrderByIdAsync(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<IEnumerable<OrderResponseDto>> GetAllOrdersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<OrderResponseDto> CreateOrderAsync(OrderCreateDto orderCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderResponseDto?> UpdateOrderAsync(Guid orderId, OrderUpdateDto orderUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<bool> DeleteOrderAsync(Guid orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
