34974709f544aa4611d8d37a7d13d39b8d572f66
fianerd <mattiasson.sofia@gmail.com>
2026-03-10 11:40:24 +0100

    feat(order-dto): Add update order dto

--- Printagon.Api/DTOs/Order/OrderUpdateDto.cs
+++ Printagon.Api/DTOs/Order/OrderUpdateDto.cs
@@ -2,5 +2,13 @@
 {
     public class OrderUpdateDto
     {
+        public int OrderNumber { get; set; }
+        public string JobName { get; set; } = string.Empty;
+        public int? YearlyNumber { get; set; }
+        public string PaperType { get; set; } = string.Empty;
+        public int GramWeight { get; set; }
+        public int RollWidth { get; set; }
+        public Enums.OrderStatus OrderStatus { get; set; }
+        public string? Comment { get; set; }
     }
 }
