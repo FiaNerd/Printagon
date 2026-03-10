025f52bb3f2d92edf07582d41fee9fa72c965fea
fianerd <mattiasson.sofia@gmail.com>
2026-03-10 11:25:31 +0100

    feat(order-service): Add Create order dto

---
+++ Printagon.Api/DTOs/Order/OrderCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using Printagon.Api.Enums;
+
+namespace Printagon.Api.DTOs.Order
+{
+    public class OrderCreateDto
+    {
+        public int OrderNumber { get; set; }
+        public string JobName { get; set; } = string.Empty;
+        public int? YearlyNumber { get; set; }
+        public string PaperType { get; set; } = string.Empty;
+        public int GramWeight { get; set; }
+        public int RollWidth { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public string? Comment { get; set; }
+
+    }
+}
