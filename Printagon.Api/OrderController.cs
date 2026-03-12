a760cadf6709272bd09c024e6cb456d9f2a612e6
fianerd <mattiasson.sofia@gmail.com>
2026-03-12 12:23:46 +0100

    feat(order-controller): add GET endpoint for retrieving all orders

---
+++ Printagon.Api/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Printagon.Api
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        // GET: api/orders
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var orders = await _orderService.GetAllOrdersAsync();
+
+            return Ok(orders);
+        }
+
+    }
+}
