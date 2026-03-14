fda7b5f6d8a27a9ab0b715d4fe6bfc7e7c7433ab
fianerd <mattiasson.sofia@gmail.com>
2026-03-14 16:35:07 +0100

    feat(order-repository): Add IRollInterface and implemented roll methods

---
+++ Printagon.Api/Repositories/Interfaces/IRollInterface.cs
@@ -0,0 +1,13 @@
+﻿using Printagon.Api.Models;
+
+namespace Printagon.Api.Repositories.Interfaces
+{
+    public interface IRollInterface
+    {
+        Task<Roll?> GetRollByIdAsync(int rollId);
+        Task<IEnumerable<Roll>> GetAllRollsAsync();
+        Task<Roll> CreateRollAsync(Roll roll);
+        Task<Roll?> UpdateRollAsync(int rollId, Roll updatedRoll);
+        Task<bool> DeleteRollAsync(int rollId);
+    }
+}
