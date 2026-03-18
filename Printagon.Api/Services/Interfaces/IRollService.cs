50f3eb30699fc54c27b8e3519c7bd2ba2f1a6591
fianerd <mattiasson.sofia@gmail.com>
2026-03-18 21:17:14 +0100

    feat(roll-service): Implemnted interace for rolls in service

---
+++ Printagon.Api/Services/Interfaces/IRollService.cs
@@ -0,0 +1,13 @@
+﻿using Printagon.Api.Models;
+
+namespace Printagon.Api.Services.Interfaces
+{
+    public interface IRollService
+    {
+        Task<IEnumerable<Roll>> GetAllRollsAsync();
+        Task<Roll?> GetRollByIdAsync(Guid rollId);
+        Task<Roll> CreateRollAsync(Roll roll);
+        Task<Roll?> UpdateRollAsync(Guid rollId, Roll updatedRoll);
+        Task<bool?> DeleteRollAsync(Guid rollId);
+    }
+}
