fcef4fe42c75ee6b7ab34fefcabcf3518589d960
fianerd <mattiasson.sofia@gmail.com>
2026-03-20 15:53:22 +0100

    feat(roll-service): Implemnted GetAllRollsAsync for fetching all rolls

---
+++ Printagon.Api/Services/RollService.cs
@@ -0,0 +1,43 @@
+﻿using Printagon.Api.Models;
+using Printagon.Api.Repositories.Interfaces;
+using Printagon.Api.Services.Interfaces;
+
+namespace Printagon.Api.Services
+{
+    public class RollService : IRollService
+    {
+        private readonly IRollRepository _rollRepo;
+
+        public RollService(IRollRepository rollRepo)
+        {
+            _rollRepo = rollRepo;
+        }
+
+        public async Task<IEnumerable<Roll>> GetAllRollsAsync()
+        {
+            return await _rollRepo.GetAllRollsAsync();
+        }
+
+        public Task<Roll?> GetRollByIdAsync(Guid rollId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Roll> CreateRollAsync(Roll roll)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+        public Task<Roll?> UpdateRollAsync(Guid rollId, Roll updatedRoll)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool?> DeleteRollAsync(Guid rollId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
