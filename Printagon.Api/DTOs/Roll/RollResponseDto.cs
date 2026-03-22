022e2d3471f82cd936fa1aa7855e053908bd99df
fianerd <mattiasson.sofia@gmail.com>
2026-03-22 23:00:56 +0100

    feat(roll-dto): Add RollResponseDto

---
+++ Printagon.Api/DTOs/Roll/RollResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Printagon.Api.DTOs.Roll
+{
+    public class RollResponseDto
+    {
+        public Guid Id { get; set; }
+        public string? PaperType { get; set; }
+        public int? GramWeight { get; set; }
+        public int? RollWidth { get; set; }
+        public int RollNumber { get; set; }
+        public int RollWeight { get; set; }
+        public int? RollWeightLeftOver { get; set; }
+        public string? Comment { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
