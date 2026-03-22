cefb4203d8c6488cff38d152f7b4393f4d45f3c3
fianerd <mattiasson.sofia@gmail.com>
2026-03-22 23:24:40 +0100

    feat(roll-dto): Add RollCreateDto

---
+++ Printagon.Api/DTOs/Roll/RollCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Printagon.Api.DTOs.Roll
+{
+    public class RollCreateDto
+    {
+        [Required]
+        public int RollNumber { get; set; }
+
+        [Required]
+        public int RollWeight { get; set; }
+
+        public int? RollWeightLeftOver { get; set; }
+        public string? Comment { get; set; }
+
+        // Optional overrides from order defaults
+        public string? PaperTypeOverride { get; set; }
+        public int? GramWeightOverride { get; set; }
+        public int? RollWidthOverride { get; set; }
+    }
+}
