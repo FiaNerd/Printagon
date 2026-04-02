18d0682986357af15ab2176a28fd0b276bdbc22b
fianerd <mattiasson.sofia@gmail.com>
2026-04-02 12:24:22 +0200

    chore(db-seeder): Add OrderRoll to seeder

--- Printagon.Api/Data/Seed/DbSeeder.cs
+++ Printagon.Api/Data/Seed/DbSeeder.cs
@@ -11,9 +11,7 @@ namespace Printagon.Api.Data.Seed
             context.Database.EnsureCreated();
 
             if (context.Orders.Any())
-            {
                 return;
-            }
 
             // ----------- ORDERS -----------
             var order1 = new Order
@@ -21,101 +19,68 @@ namespace Printagon.Api.Data.Seed
                 Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
                 OrderNumber = 123,
                 JobName = "Ica",
-                YearlyNumber = 2,
                 PaperType = "Holmen View HS",
                 PaperGramWeight = 53,
                 PaperWidth = 1144,
-                OrderStatus =  OrderStatus.Active,
-                CreatedAt = DateTime.Now,
-                Rolls = new List<Roll>()
+                OrderStatus = OrderStatus.Active,
+                CreatedAt = DateTime.Now
             };
 
-            // ----------- ORDERS -----------
             var order2 = new Order
             {
                 Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
                 OrderNumber = 234,
                 JobName = "Hemmets Journal",
-                YearlyNumber = 1,
                 PaperType = "Skogh papper",
                 PaperGramWeight = 45,
                 PaperWidth = 845,
                 OrderStatus = OrderStatus.Paused,
-                CreatedAt = DateTime.Now,
-                Rolls = new List<Roll>()
-            };
-
-            var order3 = new Order
-            {
-                Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
-                OrderNumber = 234,
-                JobName = "Hemmets Journal",
-                YearlyNumber = 5,
-                PaperType = "Skogh papper",
-                PaperGramWeight = 45,
-                PaperWidth = 845,
-                OrderStatus = OrderStatus.Completed,
-                CreatedAt = DateTime.Now,
-                Rolls = new List<Roll>()
+                CreatedAt = DateTime.Now
             };
 
             // ----------- ROLLS -----------
             var roll1 = new Roll
             {
                 Id = Guid.NewGuid(),
-                PaperType = "Holmen View HS",
-                PaperWidth = 1144,
                 RollNumber = 1,
                 RollWeight = 1000,
-                Comment = "First roll",
-                CreatedBy = Guid.NewGuid(),
-                CreatedAt = DateTime.Now,
-                Orders = new List<Order> { order1 }
+                RollWeightLeftOver = 1000,
+                PaperType = "Holmen View HS",
+                CreatedAt = DateTime.Now
             };
 
             var roll2 = new Roll
             {
                 Id = Guid.NewGuid(),
-                PaperType = "Holmen View HS",
-                PaperGramWeight = 53,
-                PaperWidth = 1144,
                 RollNumber = 2,
                 RollWeight = 1000,
-                Comment = "Second roll",
-                CreatedBy = Guid.NewGuid(),
-                CreatedAt = DateTime.Now,
-                Orders = new List<Order> { order1 }
-            };
-
-            var roll3= new Roll
-            {
-                Id = Guid.NewGuid(),
+                RollWeightLeftOver = 800,
                 PaperType = "Holmen View HS",
-                PaperGramWeight = 52,
-                PaperWidth = 845,
-                RollNumber = 3,
-                RollWeight = 1000,
-                RollWeightLeftOver = 500,
-                Comment = "Third roll",
-                CreatedBy = Guid.NewGuid(),
-                CreatedAt = DateTime.Now,
-                Orders = new List<Order> { order2 }
+                CreatedAt = DateTime.Now
             };
 
-            // Koppla ihop båda sidor
-            order1.Rolls.Add(roll1);
+            var orderRoll1 = new OrderRoll
+            {
+                Order = order1,
+                Roll = roll1,
+                UsedWeight = 200
+            };
+
+            var orderRoll2 = new OrderRoll
+            {
+                Order = order1,
+                Roll = roll2,
+                UsedWeight = 200
+            };
+
+
+            order1.OrderRolls.Add(orderRoll1);
+            order1.OrderRolls.Add(orderRoll2);
+
+
+            context.Orders.AddRange(order1, order2);
             context.Rolls.AddRange(roll1, roll2);
-            order1.Rolls.Add(roll2);
-            order2.Rolls.Add(roll3);
-
-            // Lägg till i DbContext
-            context.Orders.Add(order1);
-            context.Orders.Add(order2);
-
-
-            context.Rolls.AddRange(roll3);
 
             context.SaveChanges();
         }
     }
-}
