b39544d409de04705d01f7114c486793f02a5763
fianerd <mattiasson.sofia@gmail.com>
2026-04-02 13:11:34 +0200

    feat(order-roll-dto): Add OrderRollCreateDto for creating a new roll weight

---
+++ Printagon.Api/DTOs/OrderRoll/OrderRollCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Printagon.Api.DTOs.OrderRoll
+{
+    public class OrderRollCreateDto
+    {
+        public Guid OrderId { get; set; }
+        public Guid RollId { get; set; }
+        public int NewRollWeight { get; set; }
+    }
+}
