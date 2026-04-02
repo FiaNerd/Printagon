ebd73b872f96e1182ef2b67a064ba121d1b4524b
fianerd <mattiasson.sofia@gmail.com>
2026-04-02 13:16:31 +0200

    feat(order-roll-interface): Add IOrderRoll

---
+++ Printagon.Api/Services/Interfaces/IOrderRoll.cs
@@ -0,0 +1,13 @@
+﻿using Printagon.Api.DTOs.OrderRoll;
+
+namespace Printagon.Api.Services.Interfaces
+{
+    public interface IOrderRoll
+    {
+        Task<IEnumerable<OrderRollDto>> GetAllOrderRollsAsync();
+        Task<OrderRollDto?> GetOrderRollByIdAsync(Guid orderRollId);
+        Task<OrderRollDto> CreateOrderRollAsync(OrderRollCreateDto createOrderRoll);
+        Task<OrderRollDto?> UpdateOrderRollAsync(Guid orderRollId, OrderRollUpdateDto updatedOrderRoll);
+        Task DeleteOrderRollAsync(Guid orderRollId);
+    }
+}
--- Printagon.Api/Services/Interfaces/IRollService.cs
+++ Printagon.Api/Services/Interfaces/IRollService.cs
@@ -10,5 +10,6 @@ namespace Printagon.Api.Services.Interfaces
         Task<RollResponseDto> CreateRollAsync(Guid orerId, RollCreateDto createRoll);
         Task<RollResponseDto?> UpdateRollAsync(Guid orderId, Guid rollId, RollUpdateDto updatedRoll);
         Task DeleteRollAsync(Guid rollId);
+
     }
 }
