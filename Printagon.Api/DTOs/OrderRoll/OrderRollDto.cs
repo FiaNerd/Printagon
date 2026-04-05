ae2c325d7b8d0a9893db336630770ae4355e488e
fianerd <mattiasson.sofia@gmail.com>
2026-04-05 19:17:09 +0200

    chore(order-roll): Add more info to the OrderRollDto

--- Printagon.Api/DTOs/OrderRoll/OrderRollDto.cs
+++ Printagon.Api/DTOs/OrderRoll/OrderRollDto.cs
@@ -2,8 +2,17 @@
 {
     public class OrderRollDto
     {
-        public Guid OrderId { get; set; }
-        public Guid RollId { get; set; }
-        public int IntakeWeight { get; set; }
+            public Guid OrderId { get; set; }
+            public Guid RollId { get; set; }
+
+            public int IntakeWeight { get; set; }
+            public int? OutputWeight { get; set; }
+            public int? ConsumedWeight { get; set; }
+
+            public int RollNumber { get; set; }
+            public string? PaperType { get; set; }
+            public int? PaperGramWeight { get; set; }
+            public int? PaperWidth { get; set; }
+       
     }
 }
